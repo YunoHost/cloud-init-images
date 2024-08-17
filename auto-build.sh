5dc9c20c44fa6d2e010cf13790be80df51dc1a47
ljf <ljf@grimaud.me>
2024-08-17 18:33:45 +0200

    [wip] Change the way to grows disk

--- auto-build.sh
+++ auto-build.sh
@@ -3,19 +3,19 @@
 suite=bookworm
 suite_nb=12
 
-# Download debian genericcloud image (with cloud-init inside)
+echo "Download debian genericcloud image (with cloud-init inside)"
 rm -f debian-${suite_nb}.qcow2
 wget https://cloud.debian.org/images/cloud/${suite}/latest/debian-${suite_nb}-genericcloud-amd64.qcow2 -o debian-${suite_nb}.qcow2
 
-rm -f yunohost-${suite_nb}.qcow2
-qemu-img create -f qcow2 -o preallocation=metadata yunohost-${suite_nb}.qcow2 7G
-virt-resize --expand /dev/sda1 debian-${suite_nb}.qcow2 yunohost-${suite_nb}.qcow2
+echo "Grows image"
+cp debian-${suite_nb}.qcow2 yunohost-${suite_nb}.qcow2
+qemu-img resize yunohost-${suite_nb}.qcow2 8G
 
-# Customize the image by running install script
+echo "Customize the image by running install script"
 virt-customize -a yunohost-${suite_nb}.qcow2 --hostname yunohost --update --install 'curl' --upload './cloud.cfg:/etc/cloud/cloud.cfg' --run-command "curl https://install.yunohost.org/${suite} | bash -s -- -a -d testing"
 
-# Reduce the image's size 
+echo "Reduce the image's size"
 virt-sparsify --in-place yunohost-${suite_nb}.qcow2
 
-# Compress
+echo "Compress"
 #xz -v yunohost-${suite_nb}.qcow2
