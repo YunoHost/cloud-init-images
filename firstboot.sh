92122b9b8d3ce104d6a700e6766eeabca4d308f4
Félix Piédallu <felix@piedallu.me>
2024-10-24 16:46:36 +0200

    prettier resolv.conf writing

--- firstboot.sh
+++ firstboot.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 # Fix strange issue where the /etc/resolv.conf is a symlink onto /run/systemd.../resolv.conf
 rm -f /etc/resolv.conf
-echo 'nameserver 89.234.141.66' > /etc/resolv.conf
-echo 'nameserver 185.233.100.100' >> /etc/resolv.conf
-echo 'nameserver 2a0c:e300::101' >> /etc/resolv.conf
-echo 'nameserver 2001:910:800::40' >> /etc/resolv.conf
+cat << EOF > /etc/resolv.conf
+nameserver 89.234.141.66
+nameserver 185.233.100.100
+nameserver 2a0c:e300::101
+nameserver 2001:910:800::40
+EOF
